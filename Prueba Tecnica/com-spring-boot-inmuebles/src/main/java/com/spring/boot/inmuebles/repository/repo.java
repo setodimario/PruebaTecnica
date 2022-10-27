c439bef62ca5af93b4c16df986fc396585899fb9
sebastian.bolanos <sebastian.bolanos@arkangroupco.com>
2022-10-27 16:20:36 -0500

    proyecto actualizado

--- Prueba Tecnica/com-spring-boot-inmuebles/src/main/java/com/spring/boot/inmuebles/repository/repo.java
+++ Prueba Tecnica/com-spring-boot-inmuebles/src/main/java/com/spring/boot/inmuebles/repository/repo.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface repo extends CrudRepository<UsuarioInmuebleDto, String> {
+public interface  repo extends CrudRepository<UsuarioInmuebleDto, String> {
 
     @Query(value = "SELECT\n" +
             "  `inmobiliaria`.`usuario`.`nombre`,\n" +
